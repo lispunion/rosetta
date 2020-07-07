bd49791db74aec6dc3083af4c06ef474e4a00275
Lassi Kortela <lassi@lassi.io>
2020-07-07 10:08:56 +0300

    Add more

--- rosetta.lisp
+++ rosetta.lisp
@@ -8,32 +8,72 @@
 
 (procedure
  (purpose "Make a list of an element repeated N times")
- (in common-lisp make-list)
- (in scheme make-list))
+ (in common-lisp "(make-list N :initial-element x)")
+ (in scheme make-list)
+ (in clojure "(into () (repeat N x))"))
+
+(procedure
+ (purpose "Make a list of the given elements")
+ (in common-lisp list)
+ (in scheme list)
+ (in clojure list))
+
+(procedure
+ (purpose "Make a list of the given elements and a tail")
+ (in common-lisp list*)
+ (in scheme cons* (srfi 1))
+ (in clojure list*))
+
+;; Vectors
+
+(procedure
+ (purpose "Make a vector of an element repeated N times")
+ (in common-lisp "(make-array N :initial-element x)")
+ (in scheme make-vector)
+ (in clojure "(into [] (repeat N x))"))
+
+(procedure
+ (purpose "Make a vector of the given elements")
+ (in common-lisp vector)
+ (in scheme vector)
+ (in clojure vector))
 
 ;; Sets
 
 (procedure
  (purpose "Union of two sets")
  (in common-lisp union)
- (in scheme lset-union (srfi 1)))
+ (in scheme lset-union (srfi 1))
+ (in scheme set-union (srfi 113))
+ (in clojure union clojure.set))
 
 (procedure
  (purpose "Intersection of two sets")
  (in common-lisp intersection)
- (in scheme lset-intersection (srfi 1)))
+ (in scheme lset-intersection (srfi 1))
+ (in scheme set-intersection (srfi 113))
+ (in clojure intersection clojure.set))
 
 (procedure
  (purpose "Difference of two sets")
  (in common-lisp set-difference)
- (in scheme lset-difference (srfi 1)))
+ (in scheme lset-difference (srfi 1))
+ (in scheme set-difference (srfi 113))
+ (in clojure difference clojure.set))
 
 ;; Characters
 
 (procedure
  (purpose "Translate a Unicode character to uppercase")
  (in scheme char-upcase)
- (in common-lisp char-upcase))
+ (in common-lisp char-upcase)
+ (in clojure upper-case clojure.string))
+
+(procedure
+ (purpose "Translate a Unicode character to lowercase")
+ (in scheme char-downcase)
+ (in common-lisp char-downcase)
+ (in clojure lower-case clojure.string))
 
 ;; Strings
 
